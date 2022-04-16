71afc7bfbbb27299b1a8946fd4a65086f775d0c5
Jeff Wu <jeffywu@pm.me>
2022-04-16 05:45:22 -0700

    adding treasury manager balance, using chainlink COMP/USD oracle

--- src/comptroller.ts
+++ src/comptroller.ts
@@ -7,39 +7,48 @@ import {
 import { Comptroller, DistributedSupplierComp } from '../generated/Comptroller/Comptroller';
 import { Aggregator } from '../generated/Comptroller/Aggregator';
 import { COMPBalance, TvlHistoricalData } from '../generated/schema';
-import { createDailyTvlId, ethToUsd } from './timeseriesUpdate';
+import { createDailyTvlId } from './timeseriesUpdate';
 import { Notional } from '../generated/Notional/Notional';
 import { ADDRESS_ZERO } from './common';
+import { ERC20 } from '../generated/Notional/ERC20';
 
 const BI_DAILY_BLOCK_UPDATE = 3300;
 
 class Addresses {
     notional: Address;
     compOracle: Address;
+    treasuryManager: Address;
 }
 
 function getAddresses(network: string): Addresses {
     if (network == "goerli") {
         return {
             notional: Address.fromHexString("0xD8229B55bD73c61D840d339491219ec6Fa667B0a") as Address,
-            compOracle: Address.fromHexString("0x51D73fdd11555a5aCF0af8218264f0d96ec5fc3d") as Address
+            // This testnet oracle returns COMP/ETH
+            compOracle: Address.fromHexString("0x51D73fdd11555a5aCF0af8218264f0d96ec5fc3d") as Address,
+            treasuryManager: Address.fromHexString("0x8638f94155c333fd7087c012Dc51B0528bb06035") as Address
         }
     }
     if (network == "kovan") {
         return {
             notional: Address.fromHexString("0x0EAE7BAdEF8f95De91fDDb74a89A786cF891Eb0e") as Address,
-            compOracle: Address.fromHexString("0x9657Eb0e7c57afE5049eB8802f3811860069B31A") as Address
+            // This testnet oracle returns COMP/ETH
+            compOracle: Address.fromHexString("0x9657Eb0e7c57afE5049eB8802f3811860069B31A") as Address,
+            treasuryManager: Address.fromHexString("0x049bbb3868850AEb8d606c4080A92D02CfC0b042") as Address
         }
     }
     if (network == "mainnet") {
         return {
             notional: Address.fromHexString("0x1344A36A1B56144C3Bc62E7757377D288fDE0369") as Address,
-            compOracle: Address.fromHexString("0xD14b0FDC8Dd3a3ECFec8ad538aE1621fF6F3Dc1F") as Address
+            // This is the Chainlink COMP/USD oracle
+            compOracle: Address.fromHexString("0xdbd020caef83efd542f4de03e3cf0c28a4428bd5") as Address,
+            treasuryManager: Address.fromHexString("0x53144559c0d4a3304e2dd9dafbd685247429216d") as Address
         }
     }
     return {
         notional: ADDRESS_ZERO(),
-        compOracle: ADDRESS_ZERO()
+        compOracle: ADDRESS_ZERO(),
+        treasuryManager: ADDRESS_ZERO()
     }
 }
 
@@ -60,24 +69,31 @@ function saveCOMPBalance(timestamp: i32): void {
     let tvlHistoricalData = entity as TvlHistoricalData;
 
     let comptroller = Comptroller.bind(dataSource.address());
+    let compToken = ERC20.bind(comptroller.getCompAddress())
     let addr = getAddresses(dataSource.network());
-    let oracle = Aggregator.bind(addr.compOracle)
-    let notional = Notional.bind(addr.notional);
 
+    let oracle = Aggregator.bind(addr.compOracle)
     let compBalance = COMPBalance.load(historicalId);
     if (compBalance == null) {
         compBalance = new COMPBalance(historicalId);
-        compBalance.timestamp = timestamp;
+        compBalance.timestamp = entity.timestamp;
     }
 
-    let comp = comptroller.compAccrued(addr.notional);
-    let answer = oracle.try_latestAnswer();
-    let ethValue = BigInt.fromI32(0);
-    if (!answer.reverted)
-        ethValue = answer.value.times(comp).div(BigInt.fromI32(10).pow(18));
+    let compAmount = comptroller.compAccrued(addr.notional);
+    let compInTreasury = compToken.try_balanceOf(addr.treasuryManager)
 
-    compBalance.value = comp;
-    compBalance.usdValue = ethToUsd(notional, ethValue);
+    if (!compInTreasury.reverted) {
+        // compInTreasury may revert if the COMP token does not exist (such as on testnet)
+        compAmount = compAmount.plus(compInTreasury.value)
+    }
+
+    let answer = oracle.try_latestAnswer();
+    let usdValue = BigInt.fromI32(0);
+    if (!answer.reverted)
+        usdValue = answer.value.times(compAmount).div(BigInt.fromI32(10).pow(18));
+
+    compBalance.value = compAmount;
+    compBalance.usdValue = usdValue; // this is in 8 decimal precision
     compBalance.save()
 
     tvlHistoricalData.compBalance = compBalance.id;
