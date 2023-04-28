0cbda695cc0d3c5ab82162cc04d1937519d05927
Jeff Wu <jeff@notional.finance>
2023-04-28 12:14:45 -0700

    adding local docker compose

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+export NETWORK_NAME="arbitrum-one"
+export RPC_URL="http://localhost:8545"
+yarn run prepare:local
+yarn run codegen
+docker-compose down
+# docker-compose pull
+docker-compose up -d
+echo "Sleeping 15 seconds for the graph to start..."
+sleep 25
+yarn run deploy:local
